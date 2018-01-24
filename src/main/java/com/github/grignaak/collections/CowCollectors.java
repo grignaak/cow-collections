a8afdd43e055229a0183aa541bf9eae63c4a0b7d
tau3 <sergey.rozhnov@nominum.com>
2018-01-24 13:33:32 +0400

    fixed code review bullets

--- src/main/java/com/github/grignaak/collections/CowCollectors.java
+++ src/main/java/com/github/grignaak/collections/CowCollectors.java
@@ -1,15 +1,11 @@
 package com.github.grignaak.collections;
 
-import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public final class CowCollectors {
-    public static <T> Collector<T, ?, List<T>> toList() {
-        return new ToListCollector<>();
-    }
-
-    public static <T> Collector<T, ?, List<T>> toForkedList() {
-        return new ToForkedListCollector<>();
+    public static <T> Collector<T, ?, CowList<T>> toCowList() {
+        return Collectors.toCollection(CowArrayList::new);
     }
 
     private CowCollectors() {
--- src/test/java/com/github/grignaak/collections/CowCollectorsTest.java
+++ src/test/java/com/github/grignaak/collections/CowCollectorsTest.java
@@ -6,20 +6,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-public class ToListCollectorTest {
+public class CowCollectorsTest {
     @Test
-    public void testUsage() {
+    public void testToCowList() {
         List<Integer> numbers = Arrays.asList(-2, -1, 0, 1, 2);
 
         final List<Integer> expected = Arrays.asList(0, 1, 2);
 
-        List<Integer> actual = numbers.stream()
+        CowList<Integer> actual = numbers.stream()
                 .filter(n -> n >= 0)
-                .collect(new ToListCollector<>());
+                .collect(CowCollectors.toCowList());
 
-        assertTrue(actual instanceof CowList);
         assertEquals(expected, actual);
     }
 }
